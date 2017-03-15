015df87d93fff1b7de832b94e606c2379517a733
XD <sinclair16.js@gmail.com>
2017-03-15 01:13:27 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Need to store Franchises somehow.
+//Implement if - then loop.
+//Maybe, "If _ received, then parse xyz websites and deliver result "
+int main()
+{
+    {
+        printf("Welcome to FanPaper! \n");
+
+        printf("FanPaper will help you search for wallpapers from your favorite franchise. \n");
+
+        printf("What kind of wallpaper are you looking for?: ");
+        scanf(" ");
+    }
+    return 0;
+}
+
