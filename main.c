389e8d9ffb0b7788635e9c5fe52d0ca81260617b
XD <sinclair16.js@gmail.com>
2017-03-16 23:50:45 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Need to store Franchises somehow.
-//Implement if - then loop.
-//Maybe, "If _ received, then parse xyz websites and deliver result "
+#include <string.h>
+//Build on this from here...
+//And leave better Comments too
 int main()
+
 {
+    char USERINPUT[30];
+    char *K = "Kingdom Hearts";
     {
         printf("Welcome to FanPaper! \n");
 
         printf("FanPaper will help you search for wallpapers from your favorite franchise. \n");
 
         printf("What kind of wallpaper are you looking for?: ");
-        scanf(" ");
+        scanf("%s", USERINPUT);
+
+        if (strcmp(USERINPUT, K) != 0)
+
+        {
+            printf("Try these websites. (or add your own in Settings!) \n");
+            printf("http://theotaku.com/wallpapers/category/kingdom_hearts/ \n");
+            printf("https://www.khinsider.com/wallpapers \n");
+            //One more HERE
+        }
     }
     return 0;
 }
